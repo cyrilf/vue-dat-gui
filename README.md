4b7da19f9a6aef1e7c8b3fcfefabe9acb7b7f737
cyrilf <1302282+cyrilf@users.noreply.github.com>
2024-01-08 15:46:07 +0100

    Update the deploy Github page script

--- README.md
+++ README.md
@@ -157,6 +157,12 @@ npm run build
 npm run lint
 ```
 
+### Deploy
+
+```
+./deploy
+```
+
 ### Customize configuration
 
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:cyrilf/vue-dat-gui.git master:gh-pages
+git push -f git@github.com:cyrilf/vue-dat-gui.git main:gh-pages
 
 cd -
